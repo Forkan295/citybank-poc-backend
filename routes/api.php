ef3d708185d1e41244f3ae4f295ca6efe4cc379e
Forkan <forkanalam295@gmail.com>
2022-03-08 11:58:29 +0600

    api user controller and transaction migration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login',[UserController::class,'login']);
+Route::post('/registration',[UserController::class,'registration']);
+
+Route::get('/user',function (Request $request){
+    return response()->json(['user' => Auth::user()]);
+})->middleware('auth:api');
