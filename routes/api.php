ae1b542328ade44740835e4726659ab36640935a
dev.manjurul <mijony29@gmail.com>
2022-03-10 12:51:49 +0600

    larapass controller publish

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Auth\WebAuthnLoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[UserController::class,'login']);
 Route::post('/registration',[UserController::class,'registration']);
+
+Route::post('webauthn/login', [WebAuthnLoginController::class, 'login'])
+    ->name('webauthn.login');
 
 Route::get('/user',function (Request $request){
     if ($request->user()->tokenCan('personal-info')) {
