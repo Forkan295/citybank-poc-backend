537755ab9988d70335c77423942b30aaac957098
Al-Amin <cseal.amin.09@gmail.com>
2022-03-16 16:19:11 +0600

    update log details

--- resources/views/activity-log/show.blade.php
+++ resources/views/activity-log/show.blade.php
@@ -5,7 +5,7 @@
         <div class="px-4 sm:px-6 lg:px-8">
             <div class="sm:flex sm:items-center">
                 <div class="sm:flex-auto">
-                    <h1 class="text-xl font-semibold text-gray-900">Activiry Log Details</h1>
+                    <h1 class="text-xl font-semibold text-gray-900">Details</h1>
                 </div>
 
                 <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
@@ -17,16 +17,22 @@
                 <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                         <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                             <table class="min-w-full bg-white">
-                                @foreach ($activityLog->properties as $key => $item)
-                                    <tr class="border-t border-gray-300">
-                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">{{ $key }}</th>
-                                        <th>&nbsp;&nbsp;:&nbsp;&nbsp;</th>
-                                        <td scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">{{ ($key == 'status' && $item == 1) ? 'success' : $item }}</td>
+                            <table class="min-w-full bg-white">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-center text-sm font-semibold text-gray-900 sm:pl-6">Title</th>
+                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Value</th>
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach ($activityLog->properties as $key => $item)
+                                        <tr class="border-t border-gray-300">
+                                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">{{ $key }}</th>
+                                            <td scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm text-gray-900 sm:pl-6">{{ ($key == 'status' && $item == 1) ? 'success' : $item }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
                             </table>
-                            </div>
                         </div>
                     </div>
                 </div>
